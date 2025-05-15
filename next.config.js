dfb7d43b595173b93317875bccb4beb87c13ea45
Tooba Jatoi <toobajatoi44@gmail.com>
2025-05-16 03:11:49 +0500

    Add GitHub Pages deployment configuration

--- next.config.js
+++ next.config.js
@@ -3,16 +3,9 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'toobajatoi.github.io',
-      },
-    ],
   },
   basePath: '/portfolio',
   assetPrefix: '/portfolio/',
-  trailingSlash: true,
 }
 
 module.exports = nextConfig 
